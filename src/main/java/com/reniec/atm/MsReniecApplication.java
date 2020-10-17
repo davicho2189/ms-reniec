1c36e4198eb63f01312662cfd90661c02a6faadc
Cesar Guerrero <ce_david14@outlook.com.pe>
2020-10-16 21:32:05 -0500

    first commit

---
+++ src/main/java/com/reniec/atm/MsReniecApplication.java
@@ -0,0 +1,13 @@
+package com.reniec.atm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsReniecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsReniecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reniec/atm/MsReniecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reniec.atm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsReniecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
